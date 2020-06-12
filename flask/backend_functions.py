e5d92a4285dd7b9c680ecb6f6e6d1a8524e3b5a9
Jessica Chan <z5257686@unsw.edu.au>
2020-06-12 18:59:24 +1000

    Basic functions complete and somewhat tested

--- flask/backend_functions.py
+++ flask/backend_functions.py
@@ -1,35 +1,92 @@
 
 '''
 Database format:
-{
-    postcode: {
-        'Phone Numbers': [list of numbers]
-        'Number of Active Cases': {'Suburb': , 'Suburb': }
-        'New Active Cases': [list of suburbs with new active cases in the most recent pull of data]}
-}
+[
+    {
+        'Postcode': postcode,
+        'Phone Numbers': [list of numbers],
+        'Suburb': {
+            'New Cases': ,
+            'Cumulative Cases': ,
+        }
+        
+    },
+]
 
 '''
 
-database = {}
+global database 
+database = []
 
 
+# ==== ANALYTIC FUNCTIONS ====
+
+
+
+
+# ==== DATABASE FUNCTIONS ====
+
+# ASSUME: All valid postcodes are already in database? Or add
 def add_number(number, postcode):
-    database[postcode]['Phone Numbers'].append(number)
+    postcode_exists = False
+    for area in database:
+        if area['Postcode'] is postcode:
+            postcode_exists = True
+            area['Phone Numbers'].append(number)
+            break
+    if postcode_exists is False:
+        new_postcode = {
+            'Postcode': postcode,
+            'Phone Numbers': [number]
+        }
+        database.append(new_postcode)
+
     return {}
 
-def add_suburb()
-    database
-def update()
+
+# JACKY IS WRITING THIS
+# def add_new_case(suburb):
+#     # database['New Active Cases'].append(suburb)
+#     if suburb not in database['New Active Cases']:
+#         database['New Active Cases'].append(suburb)
 
 
+# Use this function to clear new cases for all suburbs before adding the newest iteration 
+def clear_new_active():
+    for area in database:
+        for suburb in area.keys():
+            if suburb is not 'Phone Numbers' or 'Postcode':
+                area[suburb]['New Cases'] = 0
 
-if __name__ == "__main__":
+def update_active_cases(daily_data):
+    for case in daily_data:
+        suburb = case[5]
+        postcode = case[1]
+        postcode_exists = False
+        for area in database:
+            if area['Postcode'] is postcode:
+                postcode_exists = True
+                if suburb in area.keys():
+                    area[suburb]['New Cases'] += 1
+                    area[suburb]['Cumulative Cases'] += 1
+                else:
+                    area[suburb]['New Cases'] = 1
+                    area[suburb]['Cumulative Cases'] = 1
+        if not postcode_exists:
+            new_postcode = {
+                'Postcode': postcode,
+                'Phone Numbers': [],
+                suburb: {'New Cases': 1, 'Cumulative Cases': 1}
+            }
+            database.append(new_postcode)
+
+
+if __name__ == '__main__':
     add_number('0413843630', 2036)
-
+    daily_data = [['2020-06-11', 2144.0, 'X740', 'Western Sydney', 12380.0,
+        'Cumberland (A)']]
+    update_active_cases(daily_data)
     print(database)
-
-
-
 
 # from flask import Flask
 # APP = Flask(__name__)
