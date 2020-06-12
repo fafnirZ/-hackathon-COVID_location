ab04a7a668ee83b4bc5e66746494fceb1f6543f4
Jessica Chan <z5257686@unsw.edu.au>
2020-06-12 22:14:46 +1000

    Mostly working, doesn't accumulate, yet

--- flask/backend_functions.py
+++ flask/backend_functions.py
@@ -1,33 +1,29 @@
 
+from database import get_data, get_total_cases
+import datetime
+import pandas as pd
+
 '''
 Database format:
 [
     {
         'Postcode': postcode,
         'Phone Numbers': [list of numbers],
-        'Suburb': {
-            'New Cases': ,
-            'Cumulative Cases': ,
-        }
+        'Suburb': [
+            {
+                'Suburb Name': ,
+                'New Cases': ,
+                'Cumulative Cases': ,
+            }
+        ]
         
     },
 ]
 
 '''
 
-global database 
-database = []
-
-
-# ==== ANALYTIC FUNCTIONS ====
-
-
-
-
-# ==== DATABASE FUNCTIONS ====
-
-# ASSUME: All valid postcodes are already in database? Or add
 def add_number(number, postcode):
+    database = get_data()
     postcode_exists = False
     for area in database:
         if area['Postcode'] is postcode:
@@ -37,56 +33,80 @@ def add_number(number, postcode):
     if postcode_exists is False:
         new_postcode = {
             'Postcode': postcode,
-            'Phone Numbers': [number]
+            'Phone Numbers': [number],
+            'Suburb': []
         }
         database.append(new_postcode)
 
     return {}
 
-
-# JACKY IS WRITING THIS
-# def add_new_case(suburb):
-#     # database['New Active Cases'].append(suburb)
-#     if suburb not in database['New Active Cases']:
-#         database['New Active Cases'].append(suburb)
-
-
 # Use this function to clear new cases for all suburbs before adding the newest iteration 
 def clear_new_active():
+    database = get_data()
     for area in database:
-        for suburb in area.keys():
-            if suburb is not 'Phone Numbers' or 'Postcode':
-                area[suburb]['New Cases'] = 0
+        for places in area['Suburb']:
+            places['New Cases'] = 0
 
-def update_active_cases(daily_data):
-    for case in daily_data:
-        suburb = case[5]
-        postcode = case[1]
+def suburb_in_postcode(area, suburb):
+    for places in area['Suburb']:
+        if places['Suburb Name'] is suburb:
+            return True
+    return False
+
+#NOT ACCUMULATING
+# Function that updates database for all new cases since previous run
+def update_active_cases():
+    database = get_data()
+    daily_cases = extract_daily()
+    for case in daily_cases.itertuples():
+        suburb = case.lga_name19
+        postcode = case.postcode
         postcode_exists = False
         for area in database:
             if area['Postcode'] is postcode:
                 postcode_exists = True
-                if suburb in area.keys():
-                    area[suburb]['New Cases'] += 1
-                    area[suburb]['Cumulative Cases'] += 1
-                else:
-                    area[suburb]['New Cases'] = 1
-                    area[suburb]['Cumulative Cases'] = 1
+                suburb_exists = False
+                for place in area['Suburb']:
+                    if place['Suburb Name'] is suburb:
+                        
+                        suburb_exists = True
+                        place['New Cases'] += 1
+                        place['Cumulative Cases'] += 1
+                if not suburb_exists:
+                    new_suburb = {
+                        'Suburb Name': suburb,
+                        'New Cases': 1,
+                        'Cumulative Cases': 1
+                    }
+                    area['Suburb'].append(new_suburb)
         if not postcode_exists:
             new_postcode = {
                 'Postcode': postcode,
                 'Phone Numbers': [],
-                suburb: {'New Cases': 1, 'Cumulative Cases': 1}
+                'Suburb': [{'Suburb Name': suburb, 'New Cases': 1, 'Cumulative Cases': 1}]
             }
             database.append(new_postcode)
+
+# Function that reads only the new daily data from csv as well as update the total number of cases
+def extract_daily():
+    total_cases = get_total_cases()
+    data = pd.read_csv("scraper/data.csv", skiprows=total_cases)
+    num_new_cases = pd.Index(data).size
+    # print(num_new_cases)
+    total_cases += num_new_cases
+    return data
 
 
 if __name__ == '__main__':
     add_number('0413843630', 2036)
-    daily_data = [['2020-06-11', 2144.0, 'X740', 'Western Sydney', 12380.0,
-        'Cumberland (A)']]
-    update_active_cases(daily_data)
+    update_active_cases()
+    database = get_data()
+    print(get_total_cases())
     print(database)
+    print(get_total_cases())
+    clear_new_active()
+    #print(database)
+
 
 # from flask import Flask
 # APP = Flask(__name__)
--- flask/database.py
+++ flask/database.py
@@ -1,5 +1,9 @@
 database = []
-
+total_cases = 0
 def get_data():
 	global database
 	return database
+
+def get_total_cases():
+	global total_cases
+	return total_cases
